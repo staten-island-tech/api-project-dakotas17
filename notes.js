8e3dee40d9d3e960c5f6b778a1aa4ad414e9fa31
dakotas17 <dakotas17@nycstudents.net>
2023-12-01 09:24:34 -0500

    almsotms

--- notes.js
+++ notes.js
@@ -15,17 +15,20 @@ Aaron.then((result)=>{
 }); 
 REST API
 */
-const URL =`https://api.quotable.io/pokemon`;
+const URL =`https://api.quotable.io/random`;
 async function getData(URL){
     try {
         const response = await fetch(URL);
+        if(response.status != 200){
+            throw new Error(response.statusText);
+        }
         console.log(response);
         const data = await response.json();
         console.log(data);
         document.querySelector("h1").textContent = data.content;
         document.querySelector("h2").textContent = data.author;
     } catch (error) {
-        
+        document.querySelector("h1").textContent = `couldnt find ${pokemon}`;
     }
 }
 getData(URL)
