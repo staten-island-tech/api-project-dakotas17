b39753ef56f377c9e19e9a4a2b95e668b734cf42
dakotas17 <dakotas17@nycstudents.net>
2023-12-14 09:25:03 -0500

    weaqw

--- main.js
+++ main.js
@@ -24,8 +24,52 @@ async function getData() {
 }
 getData();
 
-let dakota = {
-    name:"Dakota",
-    age:15
+let dennis = {
+    name:"Dennis", //string
+    age:15 //integer
 }
-console.log(dakota.name)
+console.log(dennis.name)
+
+let x = Array.from(dennis.name);
+console.log(x);
+for(let i = 0; i< dennis.name.length; i++){
+    console.log(dennis.nam[i]);
+}
+let i = 0;
+while( i < dennis.name.legnth){
+    console.log(dennis.name[i]);
+    i++;
+}
+
+
+function needle(name, search){
+    if(name.includes(search)) { //trade .includes with === for exact search
+        return true;        
+    } else {
+        return false;
+    }
+}
+console.log(needle(dennis.name, "D"));
+
+function sortString(name){
+    let x = [...name].sort(); ///... takes everything in an array or string and converts it to this data type
+console.log(x);
+}
+sortString(dennis.name);
+
+let x = [1, 2, 3];
+let y = [4, 5, 6];
+let z = [...x, ...y];
+console.log(z);
+
+function removeL(name) {
+    let x = Array.from(name);
+    x.splice(-1);
+    console.log(x);
+}
+function removeL(name) {
+    let x = name;
+    x.splice(-1);
+    console.log(x);
+}
+removeL(dennis.name)
